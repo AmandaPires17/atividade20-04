7346083cd0a3b52688ccf5a873bfa0ea7a66f097
AmandaPires17 <102668847+AmandaPires17@users.noreply.github.com>
2022-04-20 09:29:48 -0300

    Add files via upload

---
+++ vue-base-exercicio-1-master/src/components/MainPrincipal.vue
@@ -0,0 +1,12 @@
+<template>
+  <main id="content">
+    <article class="block">
+      <h1>Amanda</h1>
+      <h1>Amanda</h1>
+      <h1>Amanda</h1>
+      <h1>Amanda</h1>
+      <h1>Amanda</h1>
+      <h1>Amanda</h1>
+    </article>
+  </main>
+</template>
